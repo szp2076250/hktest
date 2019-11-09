49fe817e325335126660fd6c84065d26ef7bb5fa
szp <szp2076250@outlook.com>
2019-11-09 16:57:14 +0800

    a

--- HookDLL/KeyBoardHook.cpp
+++ HookDLL/KeyBoardHook.cpp
@@ -9,8 +9,8 @@
 
 #pragma comment(lib,"Dbghelp.lib")
 
-extern HMODULE gModule;
 HHOOK gHook = 0;
+extern HANDLE hProcess;				// 进程句柄
 
 //钩子回调函数
 LRESULT CALLBACK KeyboardProc(
@@ -25,11 +25,11 @@ LRESULT CALLBACK KeyboardProc(
 
 bool OnHook()
 {
-	if (gHook == 0)
-	{
-		gHook = SetWindowsHookEx(WH_MOUSE, KeyboardProc, gModule, 0);
-		return true;
-	}
+	//if (gHook == 0)
+	//{
+	//	gHook = SetWindowsHookEx(WH_MOUSE, KeyboardProc, hProcess, 0);
+	//	return true;
+	//}
 	return false;
 }
 
@@ -51,8 +51,7 @@ HINSTANCE hInst = NULL;			// 本dll实例句柄
 #pragma comment(linker, "/section:.Share,rws")
 
 
-// 全局变量
-HANDLE hProcess = NULL;				// 进程句柄
+
 //BOOL bIsInjected = FALSE;				// 是否注入完成
 typedef int (WINAPI *MsgBoxA)(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType);			// 声明一个别名 MsgBoxA
 typedef int (WINAPI *MsgBoxW)(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uType);		// 声明一个别名 MsgBoxW
@@ -148,6 +147,7 @@ void HookOn(HANDLE hProcess)
 	if (0 == dwRet || 0 == dwWrite) { /*TRACE("NewCodeW 写入失败");*/ }
 	VirtualProtectEx(hProcess, pfMsgBoxW, 5, dwOldProtect, &dwTemp);
 
+	MessageBoxA(NULL, "TEST", "TEST", MB_OK);
 }
 
 // 关闭钩子（修改 API 头 5 个字节）
@@ -212,32 +212,31 @@ BOOL InjectAllProcess()
 		if (dwPID < 100)
 			continue;
 
-		//Inject(dwPID);
+		
 		HANDLE hProcess = ::OpenProcess(PROCESS_ALL_ACCESS, 0, dwPID);
-		auto pfnAddress = (PTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle(L"Kernerl32"),"LoadLibraryW");
+		Inject(hProcess);
+	/*	auto pfnAddress = (PTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle(L"Kernel32"),"LoadLibraryA");
 		const char * path = "../Debug/InjectDLL.dll";
 		auto baseAddress = VirtualAllocEx(hProcess, 0,strlen(path)+1, MEM_COMMIT|MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-		SIZE_T sizeofWrite;
-		WriteProcessMemory(hProcess, baseAddress, path, sizeof(char), &sizeofWrite);
-		CreateRemoteThread(hProcess,NULL,0, pfnAddress, baseAddress,0,NULL);
-		//VirtualFreeEx(hProcess, baseAddress, strlen(path) + 1, MEM_RELEASE);
-		//if (nMode == 1)
-		//	Inject(dwPID, szDllPath);
-		//else
-		//	EjectDll(dwPID, szDllPath);
+		WriteProcessMemory(hProcess, baseAddress, path, strlen(path) + 1, NULL);
+		auto hRemote = CreateRemoteThread(hProcess,NULL,0, pfnAddress, baseAddress,0,NULL);
+		VirtualFreeEx(hProcess, baseAddress, strlen(path) + 1, MEM_RELEASE);
+
+		CloseHandle(hRemote);
+		CloseHandle(hProcess);*/
 	} while (Process32Next(hSnapShot, &pe));
 
 	CloseHandle(hSnapShot);
-
+	
 	return TRUE;
 }
 
 // 代码注入
-void Inject(DWORD pid)
+void Inject(HANDLE hProcess)
 {
 	//hProcess = 
 	//Inject(D);
-	HANDLE hProcess = ::OpenProcess(PROCESS_ALL_ACCESS, 0, pid);
+	//HANDLE hProcess = ::OpenProcess(PROCESS_ALL_ACCESS, 0, pid);
 	::hProcess = hProcess;
 	// 如果还没有注入
 	//if (!bIsInjected) {
@@ -302,34 +301,37 @@ void Inject(DWORD pid)
 }
 
 void Init() {
-	DWORD dwPid = ::GetCurrentProcessId();
+	//DWORD dwPid = ::GetCurrentProcessId();
 	// 获取调用 dll 的进程句柄
-	InjectAllProcess();
+	//InjectAllProcess();
+	Inject(hProcess);
+
 }
 
-//void ChangeTable() {
-//	HMODULE hModule = GetModuleHandle(NULL);
-//	ULONG size;
-//	PROC pfnOld = GetProcAddress(GetModuleHandleA("User32.dll"), "MessageBoxA");
-//
-//	PIMAGE_IMPORT_DESCRIPTOR pImport = (PIMAGE_IMPORT_DESCRIPTOR)
-//		ImageDirectoryEntryToData(hModule,true,IMAGE_DIRECTORY_ENTRY_IMPORT,&size);
-//		while (pImport->FirstThunk) {
-//			int i = 0;
-//			char * ModuleName = (char*)((BYTE*)hModule + pImport->Name);
-//			PIMAGE_THUNK_DATA pThunk = (PIMAGE_THUNK_DATA)
-//				((BYTE*)hModule + pImport->FirstThunk);
-//			while (pThunk->u1.Function)
-//			{
-//				char * Func = (char*)((BYTE*)hModule + pThunk->u1.AddressOfData + 2);
-//				PROC *ppfn = (PROC*)&pThunk->u1.Function;
-//				printf("%s\r\n",Func);
-//				if(pfnOld == *ppfn)
-//				{
-//					MessageBoxA(NULL,"找到函数","",MB_OK);
-//				}
-//				pThunk++;
-//			}
-//			pImport++;
-//		}
-//}
+void ChangeTable(HMODULE module) {
+	HMODULE hModule = GetModuleHandle(NULL);
+	
+	ULONG size;
+	PROC pfnOld = GetProcAddress(GetModuleHandleA("User32.dll"), "MessageBoxA");
+
+	PIMAGE_IMPORT_DESCRIPTOR pImport = (PIMAGE_IMPORT_DESCRIPTOR)
+		ImageDirectoryEntryToData(module,true,IMAGE_DIRECTORY_ENTRY_IMPORT,&size);
+		while (pImport->FirstThunk) {
+			int i = 0;
+			char * ModuleName = (char*)((BYTE*)module + pImport->Name);
+			PIMAGE_THUNK_DATA pThunk = (PIMAGE_THUNK_DATA)
+				((BYTE*)module + pImport->FirstThunk);
+			while (pThunk->u1.Function)
+			{
+				char * Func = (char*)((BYTE*)module + pThunk->u1.AddressOfData + 2);
+				PROC *ppfn = (PROC*)&pThunk->u1.Function;
+				printf("%s\r\n",Func);
+				if(pfnOld == *ppfn)
+				{
+					MessageBoxA(NULL,"找到函数","",MB_OK);
+				}
+				pThunk++;
+			}
+			pImport++;
+		}
+}
